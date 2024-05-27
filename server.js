552062669d3bc50cd3cc4318867eaeffd8f4c6f0
Vimal Patel <vimal.patel@insight.com>
2024-05-27 13:53:16 +1000

    adding some collections and removing axios dependency.

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const PostmanLocalMockServer = require('@jordanwalsh23/postman-local-mock-server');
 const fs = require('node:fs');
-const axios = require('axios');
 
 let options = {
   port : 3555
 }
 
 //Create the collection object.
-options.collection = JSON.parse(fs.readFileSync('./testAPI.postman_collection.json', 'utf8'));
+options.collection = JSON.parse(fs.readFileSync('./collections/CitiMockServer_BOQ.postman_collection.json', 'utf8'));
 
 //Create a new server
 let server = new PostmanLocalMockServer(options);
@@ -16,10 +15,10 @@ let server = new PostmanLocalMockServer(options);
 server.start();
 
 //Run some requests against your server
-axios.get(`http://localhost:3555`).then(res => {
-  //do something with the mocked response.
-  print(res);
-});
+// axios.get(`http://localhost:3555`).then(res => {
+//   //do something with the mocked response.
+//   print(res);
+//});
 
 //Stop the server
 //server.stop();
