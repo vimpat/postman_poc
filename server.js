c3a7de3c3ea06538a735c5edd48f1920ebac0c1e
Vimal Patel <vimal.patel@insight.com>
2024-05-27 10:50:20 +1000

    adding project

---
+++ server.js
@@ -0,0 +1,25 @@
+const PostmanLocalMockServer = require('@jordanwalsh23/postman-local-mock-server');
+const fs = require('node:fs');
+const axios = require('axios');
+
+let options = {
+  port : 3555
+}
+
+//Create the collection object.
+options.collection = JSON.parse(fs.readFileSync('./testAPI.postman_collection.json', 'utf8'));
+
+//Create a new server
+let server = new PostmanLocalMockServer(options);
+
+//Start the server
+server.start();
+
+//Run some requests against your server
+axios.get(`http://localhost:3555`).then(res => {
+  //do something with the mocked response.
+  print(res);
+});
+
+//Stop the server
+//server.stop();
